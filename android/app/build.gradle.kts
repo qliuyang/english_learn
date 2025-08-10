24e4cc64a6798cee0946883fe26a5edac5ee6d89
qliuyang <ly599575461@outlook.com>
2025-08-10 20:29:28 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
